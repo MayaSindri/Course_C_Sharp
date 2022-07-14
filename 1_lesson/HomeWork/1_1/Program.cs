ffdb923c4499d2f0956bc6bf0d1cc32a10fdb886
Alina <linfiaflora@mail.ru>
2022-07-14 23:27:43 +0300

    сделали домашнее задание задачу 1

---
+++ 1_lesson/HomeWork/1_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+
+Console.WriteLine("Write a number a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write a number b: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b)
+Console.WriteLine("Max = a");
+else
+Console.WriteLine("Max = b");
